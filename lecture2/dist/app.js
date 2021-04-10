b188b2235c9eca26dc372f9c58e55f7dc32383cc
Amen0920 <luj0605@gmail.com>
2021-04-10 19:22:03 +0900

    commit

---
+++ lecture2/dist/app.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./client.jsx":
+/*!********************!*\
+  !*** ./client.jsx ***!
+  \********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module 'babel-core'\\nRequire stack:\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\loader-runner\\\\lib\\\\loadLoader.js\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\webpack\\\\lib\\\\NormalModuleFactory.js\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\webpack\\\\lib\\\\Compiler.js\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\webpack\\\\lib\\\\webpack.js\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\webpack\\\\lib\\\\index.js\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\webpack-cli\\\\lib\\\\webpack-cli.js\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\webpack-cli\\\\lib\\\\bootstrap.js\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\webpack-cli\\\\bin\\\\cli.js\\n- C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\webpack\\\\bin\\\\webpack.js\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:965:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:841:27)\\n    at Module.require (internal/modules/cjs/loader.js:1025:19)\\n    at require (C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:159:20)\\n    at Object.<anonymous> (C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js:3:13)\\n    at Module._compile (C:\\\\Users\\\\LEE HEEDO\\\\Desktop\\\\강의\\\\React_basic_study\\\\lecture2\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1157:10)\\n    at Module.load (internal/modules/cjs/loader.js:985:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:878:14)\\n    at Module.require (internal/modules/cjs/loader.js:1025:19)\");\n\n//# sourceURL=webpack://lecture2/./client.jsx?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./client.jsx"]();
+/******/ 	
+/******/ })()
+;
--- lecture2/webpack.config.js
+++ lecture2/webpack.config.js
@@ -1,3 +1,35 @@
+const path = require('path');
+
 module.exports = {
-    
+    name : 'wordrelay-setting',
+    mode : 'development', //실서비스에서는 production
+    devtool : 'eval',
+    resolve:{
+        extensions:['.js','.jsx'],
+        fallback: { "url": require.resolve("url/"),  "util": false , "path" :false ,"os":false ,
+        "http":false, "vm" :false, "https":false, "crypto":false, "constants":false ,"stream":false,"buffer":false},
+    },
+
+    entry:{
+        // app: ['./client.jsx', './WordRelay.jsx']
+        app:['./client']
+    }, // 입력 
+
+    module:{
+        rules:[{
+            test:/\.jsx?/,
+            loader:'babel-loader',
+            options:{
+                presets:['@babel/preset-env', '@babel/preset-react'],
+                plugins:['@babel/plugin-proposal-class-properties'],
+            }
+        }]
+    },
+
+    output:{
+        path : path.join(__dirname, 'dist'),
+        filename : 'app.js'
+    }, //출력 
+
+
 }
