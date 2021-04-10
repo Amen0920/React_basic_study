ca2babf1a75dc5aa3b46031bc315411cc65abbea
Amen0920 <luj0605@gmail.com>
2021-04-10 18:04:37 +0900

    commit

--- lecture2/client.jsx
+++ lecture2/client.jsx
@@ -1,2 +1,7 @@
 const React = require('react');
 const ReactDom = require('react-dom');
+
+const WordRelay = require('./WordRealay');
+
+
+ReactDom.render(<WordRelay /> ,document.querySelector('#root'))
