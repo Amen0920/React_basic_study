d1993f14bfc89ee88e6ecd04008817bcb1a088cb
Amen0920 <luj0605@gmail.com>
2021-04-13 14:52:47 +0900

    commit

--- 2. word-relay/WordRelay.jsx
+++ 2. word-relay/WordRelay.jsx
@@ -3,10 +3,52 @@ const { render } = require('react-dom');
 
 class WordRelay extends React.Component {
     state = {
-        text:'Hello Webpack!!',
+        word:"리베",
+        value:'',
+        result:'',
     };
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        if(this.state.word[this.state.word.length-1] == this.state.value.charAt(0)){
+            
+            this.setState({
+                result:'딩동댕!',
+                word: this.state.value,
+                value:'',
+               
+            });
+            this.input.focus();
+        }else{
+            console.log(this.state.word[this.state.word.length-1]+','+this.state.value)
+            this.setState({
+            
+                result:'땡!',
+                value:'',
+            });
+            this.input.focus();
+        }
+    }
+
+    onChangeInput = (e) => {
+        this.setState({value : e.target.value })
+    };
+
+    input;
+    onRefInput = (c) =>{
+       this.input = c;
+    }
+
     render(){
-        return (<h1>{this.state.text}</h1>)
+        return (
+            <>
+                <div>{this.state.word}</div>
+                <form onSubmit={this.onSubmitForm}>
+                    <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput}/>
+                    <button>입력!</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
+        )
     }
 
 };
