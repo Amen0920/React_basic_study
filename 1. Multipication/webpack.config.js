4e567c933edaa175dc3538999d144b84649e97d1
Amen0920 <luj0605@gmail.com>
2021-04-13 14:15:04 +0900

    commit

--- 1. Multipication/webpack.config.js
+++ 1. Multipication/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
     mode: 'development',
@@ -10,23 +11,22 @@ module.exports = {
     entry: {
         app: ['./client']
     },
+
     module: {
         rules:[{
-            test: /\.jsx$/,
+            test: /\.jsx?$/,
             loader:'babel-loader',
             options:{
-                presets:['@babel/preset-env','@babel/preset-react']
-                // presets:[ 
-                //     ['@babel/preset-env', {
-                //         targets: {
-                //             browsers: ['> 5% in KR', 'last 2 chrome versions'],
-                //         }
-                //     }],
-                //     '@babel/preset-react'],
-                // plugins:[],
+                presets:['@babel/preset-env','@babel/preset-react'],
+                plugins:[],
             }
         }]
     },
+
+    plugins:[
+        new webpack.LoaderOptionsPlugin({ debug : true })
+    ],
+    
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'app.js'
