3a8c297900e00c6316c132fcfb0fe3625ba15d49
Amen0920 <luj0605@gmail.com>
2021-04-13 13:39:36 +0900

    commit

---
+++ 1. Multipication/client.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const Multipication = require('./multipication');
+
+
+ReactDOM.render(<Multipication />,document.querySelector('#root'))
