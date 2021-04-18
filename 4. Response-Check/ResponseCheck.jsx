bd931f3b1b21ac93595e7613ae740298fa147e26
Amen0920 <luj0605@gmail.com>
2021-04-18 21:07:42 +0900

    commit;

---
+++ 4. Response-Check/ResponseCheck.jsx
@@ -0,0 +1,36 @@
+const React = require('react');
+const { Component } = React;
+
+class ResponseCheck extends Component {
+    state ={
+        state:'waiting',
+        message:'클릭해서 시작하세요',
+        result: [],
+
+    };
+    onClickScreen = () => {
+
+    }
+    renderAverage = () => {
+        const { result } = this.state;
+        return result.length === 0 
+        ? null 
+        : <div>평균시간 {this.state.result.reduce((a,c)=> a+c) / this.state.result.length} ms</div>
+    }
+
+    render(){
+        const { state, message} = this.state;
+        return(
+            <>
+                <div id="screen" className={state} onClick={this.onClickScreen}>
+                    {message}
+                </div>
+                {this.renderAverage()}
+                
+            </>
+        )
+    }
+}
+
+
+module.exports = ResponseCheck;
--- 4. Response-Check/webpack.config.js
+++ 4. Response-Check/webpack.config.js
@@ -1,5 +1,40 @@
-const Path = require('path');
+const path = require('path');
+const RefreshWebpack = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports ={
-    
+    name : 'Response-Test-Setting',
+    mode : 'development',
+    devtool: 'inline-source-map',
+    resolve:{
+        extensions:['.js','.jsx']
+    },
+
+    entry:{
+        app:['./client']
+    },
+    module:{
+        rules:[{
+            test:/\.jsx?/,
+            loader:'babel-loader',
+            options:{
+                presets:['@babel/preset-env','@babel/preset-react'],
+                plugins:['react-refresh/babel','@babel/plugin-proposal-class-properties'],
+
+            }
+        }]
+    },
+    plugins:[
+        new RefreshWebpack()
+    ],
+    output:{
+        path:path.join(__dirname,'dist'),
+        filename:'app.js',
+        publicPath:'/dist/'
+    },
+    devServer:{
+        publicPath:'/dist/',
+        port:8081,
+        hot:true
+    }
+
 }
